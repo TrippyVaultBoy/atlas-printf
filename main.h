e1e811a10a506081dd8a476812e3c5deba05db4b
Tanner Saint <tsaint2004@gmail.com>
2024-07-01 17:44:20 +0000

    added main.c main.h printf.c

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int _putchar(char c);
+int _puts(const char *str);
+int _printf(const char *format, ...);
+
+#endif
---
+++ printf.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/* compile with: gcc -Wall -Wextra -Werror -pedantic -std=gnu89 -Wno-format *.c */
+
+int _putchar(char c) {
+	return write(1, &c, 1);
+}
+
+int _puts(const char *str) {
+	int count;
+
+	count = 0;
+
+	while (*str) {
+		_putchar(*str++);
+		count++;
+	}
+	return count;
+}
+
+int _printf(const char *format, ...) {
+	int count;
+	const char *p = format;
+	
+	va_list args;
+    va_start(args, format);
+
+	count = 0;
+
+	for (; *p != '\0'; p++) {
+		if (*p == '%') {
+			p++;
+			if (*p == 'c') {
+				int c = va_arg(args, int);
+				_putchar(c);
+				count++;
+				break;
+			}
+			else if (*p == 's') {
+				char *s = va_arg(args, char *);
+				count += _puts(s);
+				break;
+			}
+			else if (*p == '%') {
+				_putchar('%');
+				count++;
+			}
+		}
+	}
+
+	va_end(args);
+	return count;
+}
