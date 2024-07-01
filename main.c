7c12187a3f1a363e6768a169f22fd0c5aba560cc
Tanner Saint <tsaint2004@gmail.com>
2024-07-01 18:45:51 +0000

    changed to switch statement

--- main.c
+++ main.c
@@ -6,21 +6,24 @@
  *
  * Return: 0 on success, error code otherwise
  */
-int main(void)
-{
+int main(void) {
 	int len, len2;
-	len = _printf("%c", 'S');
-	len2 = printf("%c", 'S');
+	len = _printf("%c\n", 'S');
+	len2 = printf("%c\n", 'S');
 	fflush(stdout);
 	if (len != len2)
 	{
 		printf("Lengths differ.\n");
+        printf("%d\n", len);
+        printf("%d\n", len2);
 		fflush(stdout);
 		return (1);
 	}
     else
     {
         printf("Lengths are equal.\n");
+        printf("%d\n", len);
+        printf("%d\n", len2);
         fflush(stdout);
         return (0);
     }
--- printf.c
+++ printf.c
@@ -13,7 +13,6 @@ int _puts(const char *str) {
 
 	while (*str) {
 		_putchar(*str++);
-		_putchar('\n');
 		count++;
 	}
 	return count;
@@ -30,27 +29,36 @@ int _printf(const char *format, ...) {
 
 	for (; *p != '\0'; p++) {
 		if (*p == '%') {
-			p++;
-			if (*p == 'c') {
-				int c = va_arg(args, int);
-				_putchar(c);
-				count++;
-				break;
-			}
-			else if (*p == 's') {
-				char *s = va_arg(args, char *);
-				count += _puts(s);
-				break;
-			}
-			else if (*p == '%') {
-				_putchar('%');
-				count++;
-			}
-			else
-			{
-				_putchar(*p);
-				count++;
-			}
+            p++;
+            switch (*p) {
+                case 'c': {
+                    int c = va_arg(args, int);
+                    _putchar(c);
+                    count++;
+                    break;
+                }
+                case 's': {
+                    char *s = va_arg(args, char *);
+                    count += _puts(s);
+                    break;
+                }
+                case '%': {
+                    _putchar('%');
+                    count++;
+                    break;
+                }
+                default: {
+                    _putchar('%');
+                    _putchar(*p);
+                    count += 2;
+                    break;
+                }
+            }
+        }
+		else
+		{
+			_putchar(*p);
+			count++;
 		}
 	}
 
